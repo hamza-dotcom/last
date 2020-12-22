5dee2fe923ed5ea9231f21d87209e37d9de26ace
hamza-dotcom <63553390+hamza-dotcom@users.noreply.github.com>
2020-12-22 21:50:18 +0100

    Update zaza.py

--- zaza.py
+++ zaza.py
@@ -1,6 +1,6 @@
 import Adafruit_DHT
 sensor = Adafruit_DHT.DHT11
-DHT11_pin = 23
+DHT11_pin = 4
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
 if humidity is not None and temperature is not None:
